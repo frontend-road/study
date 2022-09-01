c402c4fcc42917f659fe1b4dbe7328313d04a942
root <root@squirrel.localdomain>
2022-09-01 21:09:41 +0800

    test

--- docs/.vuepress/client.ts
+++ docs/.vuepress/client.ts
@@ -1,17 +1,17 @@
 import { defineClientConfig } from '@vuepress/client'
-import columns from './public/geektime/column/columns.json'
+// import columns from './public/geektime/column/columns.json'
 
 // https://v2.vuepress.vuejs.org/zh/advanced/cookbook/usage-of-client-config.html
 export default defineClientConfig({
   enhance({ app, router, siteData }) {
     // console.log('client:', app, router, siteData)
-    app.mixin({
-      computed: {
-        columns: function() {
-          return columns
-        },
-      }
-    })
+    // app.mixin({
+    //   computed: {
+    //     columns: function() {
+    //       return columns
+    //     },
+    //   }
+    // })
   },
   setup() {},
   rootComponents: [],
