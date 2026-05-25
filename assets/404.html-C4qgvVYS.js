86234ed2db7600a72bb088d2ad7c331908f776d8
yusong.zhou <yusong.zhou@czayd.top>
2026-05-25 18:16:30 +0800

    deploy

---
+++ assets/404.html-C4qgvVYS.js
@@ -0,0 +1,1 @@
+import{c as e,h as t,t as n,u as r}from"./app-CtjxNJzU.js";var i=JSON.parse(`{"path":"/404.html","title":"","lang":"zh-CN","frontmatter":{"layout":"NotFound"},"git":{},"filePathRelative":null}`),a={name:`404.html.vue`};function o(n,i,a,o,s,c){return t(),r(`div`,null,[...i[0]||=[e(`p`,null,`404 Not Found`,-1)]])}var s=n(a,[[`render`,o]]);export{i as _pageData,s as default};
